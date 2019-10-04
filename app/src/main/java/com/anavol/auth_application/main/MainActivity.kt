6148917d5d9af90e29e79f2d9e87569a1b4d84b9
Anavol <Anavol777@gmail.com>
2019-10-04 18:37:07 +0300

    git search added with delay

--- app/src/main/java/com/anavol/auth_application/main/MainActivity.kt
+++ app/src/main/java/com/anavol/auth_application/main/MainActivity.kt
@@ -3,7 +3,6 @@ package com.anavol.auth_application.main
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -22,7 +21,6 @@ import com.anavol.auth_application.gitSearchTools.GithubApiService
 import com.anavol.auth_application.login.LoginActivity
 import com.anavol.auth_application.login.User
 import com.anavol.auth_application.searchRecycleView.CustomAdapter
-import com.anavol.auth_application.searchRecycleView.GitUserRecyclerAdapter
 import com.anavol.auth_application.userDBTools.DbTools
 import com.anavol.auth_application.userDBTools.UserDataBase
 import com.squareup.picasso.Picasso
@@ -31,25 +29,27 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var viewModel: MainViewModel
+    private lateinit var searchString: String
     private val gitApiServe by lazy {
         GithubApiService.create()
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val user = intent.getParcelableExtra<User>("user")
         val mDb = UserDataBase.getInstance(this)
+        var page = 0
         val loginIntent = Intent(this, LoginActivity::class.java)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(
-            this, R.layout.activity_main
+            this, com.anavol.auth_application.R.layout.activity_main
         )
         val navBind: NavHeaderMainBinding = NavHeaderMainBinding.inflate(
             layoutInflater,
@@ -61,17 +61,32 @@ class MainActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
         navBind.viewModel = viewModel
 
-        val recyclerView = recyclerView as RecyclerView
-        //adding a layoutmanager
+        val recyclerView = recyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
-        //crating an arraylist to store users using the data class user
         val users = ArrayList<GitUser>()
-
-        //creating our adapter
         val adapter = CustomAdapter(users)
-
-        //now adding the adapter to recyclerview
         recyclerView.adapter = adapter
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+        if (recyclerView.layoutManager?.childCount?.plus((recyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition())!!
+            == recyclerView.layoutManager?.itemCount?.minus(5)!! )
+                   {  //if near fifth item from end
+                    searchString = searchField.text.toString()
+                    gitApiServe.search(searchString, page++, 30)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                            { result ->
+                                users.addAll(result.items)
+                                adapter.notifyDataSetChanged()
+                            },
+                            { error -> val err = error.message
+                            }
+                        )
+                }
+            }
+        })
 
         val contentViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         val contentBind: ContentMainBinding = DataBindingUtil.inflate(
@@ -85,7 +100,7 @@ class MainActivity : AppCompatActivity() {
         viewModel.photo.value = user.photo
 
         Picasso.get()
-            .load(viewModel.photo.value)//
+            .load(viewModel.photo.value)
             .into(navBind.profilePic)
         viewModel.login.value = user.login
 
@@ -103,10 +118,15 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }
-
         btnSearch.setOnClickListener {
-            val searchString = searchField.text.toString()
-            gitApiServe.search(searchString, 0, 30)
+
+            users.clear()
+            page = 0
+            adapter.notifyDataSetChanged()
+            searchString = searchField.text.toString()
+            GlobalScope.launch(Dispatchers.Main) {
+                delay(600)
+            gitApiServe.search(searchString, page++, 30)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
@@ -115,28 +135,11 @@ class MainActivity : AppCompatActivity() {
                         adapter.notifyDataSetChanged()
                     },
                     { error -> val er = error.message
-                        Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
                     }
                 )
         }
+        }
     }
-
-    private fun beginSearch(searchString: String, users: ArrayList<GitUser>) {
-        gitApiServe.search(searchString, 0, 100)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                { result ->
-                    users.addAll(result.items)
-                },
-                { error ->
-                    Toast.makeText(this, error.message, Toast.LENGTH_SHORT).show()
-                }
-            )
-
-    }
-
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
